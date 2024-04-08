e57f1d56c12b0497b84514e4d32c7cf7f7f1f1e2
TejasMars <tejas.pawar@effem.com>
2024-04-08 17:40:29 +0100

    minor fix

--- app/headTag.jsx
+++ app/headTag.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const HeadTag = () => {
   return (
     <head>
-        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'/>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover, maximum-scale=1'/>
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
